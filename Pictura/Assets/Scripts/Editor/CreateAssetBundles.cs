7b1680afb334017bd71ca4013ae82025cbb580a1
dave_mm0 <david.menard0@gmail.com>
2020-05-10 18:18:46 -0400

    Initial Commit

---
+++ Pictura/Assets/Scripts/Editor/CreateAssetBundles.cs
@@ -0,0 +1,37 @@
+﻿
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles : EditorWindow {
+    
+    private string savePath = "C:/Pictura Bundles";
+
+    void OnEnable() {
+        
+    }
+
+    [MenuItem("Pictura/Build Asset Bundles")]
+    public static void ShowWindow() {
+        EditorWindow.GetWindow(typeof(CreateAssetBundles));
+    }
+
+    void OnGUI() {
+        GUILayout.Label("Build Folder", EditorStyles.boldLabel);
+        savePath = EditorGUILayout.TextField("Build Folder", savePath);
+
+        if (GUILayout.Button("Build")) {
+            if (!Directory.Exists(savePath)) {
+                Directory.CreateDirectory(savePath);
+            }
+            BuildPipeline.BuildAssetBundles(savePath,
+                                            BuildAssetBundleOptions.None,
+                                            BuildTarget.StandaloneWindows);
+        }
+    }
+}
+
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Pictura
 This repository contains everything you need to create a custom level for Pictura
+
+## Getting Started
+1. Download Unity Hub (optional)
+2. Add the "Pictura" project to the Unity Hub. From there, the Hub ill tell you that you don't thave the correct versino of Unity to Open the project, adnwill suggest you download & install it. Go ahead and do that.
+3. The correct version of Unity is 2020.1.0b5.3485. Make sure you have this specific version installed.
+4. Open the "Pictura" project in Unity. 
+5. Frmo here, the easiest way to get started is to make a copy of the "SampleScene" found in the Assets/Scenes folder and go from there!
+
+##Building a level
+
